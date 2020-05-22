b4aaf111de6626c454345dbc30985900fd230714
https://github.com/ayomidebajo <oluwashinabajo@gmail.com>
2020-05-22 12:10:49 +0100

    Remove obselete code

--- src/components/home/header.js
+++ src/components/home/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./navbar";
-// import  img  from "../../img/cab.png";
+
 
 
 
@@ -16,7 +16,7 @@ function header() {
           <h1>Book Your Cab Now</h1>
           <h2>+1(12) 366 411 4999</h2>
           <p>Always Available, Best Cabs, Safe Journey.</p>
-          {/* <img src={img} alt="cab" width="100%" height="100%"/> */}
+         
         </div>
       </div>
     </div>
